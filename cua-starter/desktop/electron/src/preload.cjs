bb0bb283f9946539c7b42b72dcc20546ebcd261a
udaybuilds47 <udaybuilds@gmail.com>
2025-08-10 19:44:52 -0500

    trash

--- cua-starter/desktop/electron/src/preload.cjs
+++
@@ -1,5 +0,0 @@
-const { contextBridge } = require("electron");
-contextBridge.exposeInMainWorld("cua", {
-  // Modern approach: WebContentsView will be embedded in the renderer
-  // No need for IPC navigation
-});
