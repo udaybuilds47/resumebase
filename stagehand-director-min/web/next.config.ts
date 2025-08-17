b6ca6b84ee8e9f5316c17a0945e4943ba7315d32
udaybuilds47 <udaybuilds@gmail.com>
2025-08-17 12:48:27 -0500

    clean

--- stagehand-director-min/web/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_SERVER_URL: process.env.SERVER_URL,
-  },
-};
-
-export default nextConfig;
--- stagehand-director-min/web/src/types/css.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.css" {
-  const content: any;
-  export default content;
-}
-
-declare module "rrweb-player/dist/style.css" {
-  const content: any;
-  export default content;
-}
